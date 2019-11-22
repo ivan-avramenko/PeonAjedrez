bf40c5e275357f9733b0d6564b11e328bc9f19fb
Ivan <Ivan@desctop>
2019-11-22 18:19:39 +0100

    11. clase Peon

---
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -0,0 +1,12 @@
+package org.iesalandalus.programacion.peonajedrez;
+
+public class Peon {
+	/*
+	 * 11. Crea la clase Peon, dentro del paquete adecuado, cuyos atributos serán un
+	 * color (del tipo enumerado Color) y posicion (de la clase Posicion), con la
+	 * visibilidad adecuada. Haz un commit.
+	 */
+	private Color color;
+	private Posicion posicion;
+
+}
