1fc03479d143d139eba2c69005511a587438810a
Ivan <Ivan@desctop>
2019-11-22 18:03:32 +0100

    9. metodos equals y hashCode

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -59,4 +59,34 @@ public class Posicion {
 		this.columna = copia.columna;
 
 	}
+
+	/*
+	 * 9. Crea los métodos equals y hashCode para esta clase. Haz un commit.
+	 */
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + columna;
+		result = prime * result + fila;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		if (columna != other.columna)
+			return false;
+		if (fila != other.fila)
+			return false;
+		return true;
+	}
+	
 }
