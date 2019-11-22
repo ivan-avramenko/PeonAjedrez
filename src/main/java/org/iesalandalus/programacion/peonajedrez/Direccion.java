3dd1be091e77147c7066af86a9407f32e8315d18
Ivan <Ivan@desctop>
2019-11-22 16:10:32 +0100

    4. Enumerado Direccion

---
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Direccion.java
@@ -0,0 +1,8 @@
+package org.iesalandalus.programacion.peonajedrez;
+
+public enum Direccion {
+/*
+ * 4. Crea un enumerado llamado Direccion, dentro del paquete adecuado, que contenga los valores: IZQUIERDA y DERECHA. Haz un commit.
+ * */
+	IZQUIERDA, DERECHA; 
+}
