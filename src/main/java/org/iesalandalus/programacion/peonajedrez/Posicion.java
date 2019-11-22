f50b0e79067470e1e2bd877eb5aef372e39fcdf3
Ivan <Ivan@desctop>
2019-11-22 17:55:21 +0100

    8. constructor copia

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -36,16 +36,27 @@ public class Posicion {
 		}
 		this.columna = columna;
 	}
+
 	/*
-	 * 7. Crea un constructor para esta clase que acepte como parámetros la fila y la 
-	 * columna y que los asigne a los atributos si son correctos y si no lance una excepción
-	 * del mismo tipo que la anterior indicando el problema. Para ello utiliza los métodos set
-	 * anteriormente creados. Haz un commit.
-	 * */
-	public Posicion (int fila, char columna) {
+	 * 7. Crea un constructor para esta clase que acepte como parámetros la fila y
+	 * la columna y que los asigne a los atributos si son correctos y si no lance
+	 * una excepción del mismo tipo que la anterior indicando el problema. Para ello
+	 * utiliza los métodos set anteriormente creados. Haz un commit.
+	 */
+	public Posicion(int fila, char columna) {
 		setFila(fila);
 		setColumna(columna);
 	}
-	
-	
+
+	/*
+	 * 8. Crea el constructor copia para esta clase. Haz un commit.
+	 */
+
+	public Posicion(Posicion copia) {
+		if (copia == null)
+			throw new NullPointerException("ERROR: No se puede copiar una Posicion nula.");
+		this.fila = copia.fila;
+		this.columna = copia.columna;
+
+	}
 }
