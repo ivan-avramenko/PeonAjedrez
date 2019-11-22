14adbd9ff89e0b5f9db21c2731b873c79f24b361
Ivan <Ivan@desctop>
2019-11-22 19:03:04 +0100

    14. constructor

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -40,12 +40,31 @@ public class Peon {
 	}
 
 	/*
-	 * 13. Crea un constructor por defecto para esta clase que cree un peón negro en la
-	 * posición '7d'. Haz un commit.
+	 * 13. Crea un constructor por defecto para esta clase que cree un peón negro en
+	 * la posición '7d'. Haz un commit.
 	 */
 
 	public Peon() {
 		this.color = Color.NEGRO;
 		this.posicion = new Posicion(7, 'd');
 	}
+
+	/*
+	 * 14. Crea un constructor para la clase que acepte como parámetro el color y
+	 * que creará un peón de dicho color cuya posición será '2d' si es blanco o '7d'
+	 * si es negro. Haz un commit.
+	 */
+	public Peon(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No es posible asignar un color nulo");
+		}
+		if (color == Color.BLANCO) {
+			this.posicion = new Posicion(2, 'd');
+		} else {
+			if (color == Color.NEGRO) {
+				this.posicion = new Posicion(7, 'd');
+			}
+		}
+	}
+
 }
