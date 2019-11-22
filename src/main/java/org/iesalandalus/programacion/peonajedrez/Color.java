37608a3c3082e2768a90f5d3ce379de0cb3c3ada
Ivan <Ivan@desctop>
2019-11-22 16:08:13 +0100

    3. Enumerado Color

---
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Color.java
@@ -0,0 +1,8 @@
+package org.iesalandalus.programacion.peonajedrez;
+
+public enum Color {
+/*
+ * 3. Crea un enumerado llamado Color, dentro del paquete adecuado, que contenga los valores: BLANCO y NEGRO. Haz un commit.
+ * */
+	BLANCO, NEGRO;
+}
