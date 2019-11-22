c01c3f188def500f452cf7db082e9638509562c7
Ivan <Ivan@desctop>
2019-11-22 16:18:03 +0100

    5. clase Posicion

---
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.peonajedrez;
+
+public class Posicion {
+/*
+ * 5. Crea la clase Posicion. Crea los atributos fila (int) y columna (char) con la visibilidad adecuada. Haz un commit.
+ * */
+	private int fila;
+	private char columna; 
+	
+}
