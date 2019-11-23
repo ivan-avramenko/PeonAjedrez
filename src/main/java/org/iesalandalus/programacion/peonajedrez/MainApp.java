c46795414c459bc12179e8a04f93ca126e5da118
Ivan <Ivan@desctop>
2019-11-23 16:19:08 +0100

    20. iii elegirOpcion()

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -2,20 +2,43 @@ package org.iesalandalus.programacion.peonajedrez;
 
 public class MainApp {
 
-	private static Peon peon; 
-	
+	private static Peon peon;
+
 	public static void main(String[] args) {
 		System.out.println("Programa para aprender a colocar y mover un peón en el tablero de ajedrez");
 		System.out.println("-------------------------------------------------------------------------");
 		System.out.println("Hasta luego Lucas!!!!");
 	}
+
 	/*
 	 * 20. i. void mostrarPeon(): mostrará por consola la representación del peón
 	 * representado por el atributo de clase peon (crea el atributo si aún no lo has
 	 * hecho)
 	 */
 	private static void mostrarPeon() {
-		
+
 	}
 
+	/*
+	 * 20. ii. void mostrarMenu(): mostrará por consola el menú con las diferentes
+	 * opciones de nuestro programa.
+	 */
+
+	private static void mostrarMenu() {
+		System.out.println("Mostrar menu :");
+
+	}
+
+	/*
+	 * 20 iii. int elegirOpción(): Nos mostrará un mensaje para que elijamos una
+	 * opción del menú anteriormente creado y nos pedirá que introduzcamos por
+	 * teclado la opción hasta que ésta sea valida. Devolverá la opción elegida.
+	 */
+
+	private static void elegirOpcion() {
+		System.out.println("Eliga una opcion :");
+
+	}
+
+	
 }
