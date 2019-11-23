f6b58360784164aa1aaa41eb88b36ed03092aab1
Ivan <Ivan@desctop>
2019-11-23 12:35:46 +0100

    20. i. mostrarPeon()

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -1,12 +1,21 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 public class MainApp {
+
+	private static Peon peon; 
 	
 	public static void main(String[] args) {
 		System.out.println("Programa para aprender a colocar y mover un peón en el tablero de ajedrez");
 		System.out.println("-------------------------------------------------------------------------");
-		
 		System.out.println("Hasta luego Lucas!!!!");
 	}
-	
+	/*
+	 * 20. i. void mostrarPeon(): mostrará por consola la representación del peón
+	 * representado por el atributo de clase peon (crea el atributo si aún no lo has
+	 * hecho)
+	 */
+	private static void mostrarPeon() {
+		
+	}
+
 }
